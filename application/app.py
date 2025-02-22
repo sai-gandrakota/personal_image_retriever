095e9fe50cee16dc356dc3e685709c5c7f25303f
sai-gandrakota <i-sai.gandrakota@affine.ai>
2025-02-21 22:32:17 -0500

    Update app.py

--- application/app.py
+++ application/app.py
@@ -9,9 +9,7 @@ from rapidfuzz import fuzz, process
 from utils import get_text_embedding, load_model
 from utils import load_from_pickle, compute_rrf
 
-# Ensure proper handling of parallel processing issues
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-os.environ["QAI_HUB_AUTO_CLONE"] = "1" 
+
 # Set the device (use GPU if available, otherwise CPU)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
